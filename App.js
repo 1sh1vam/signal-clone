55f5ebe21fd8f629434a4f731d923598233949ae
1sh1vam <shivam07.mt@gmail.com>
2023-01-14 19:14:41 +0530

    login screen and adding the react native elements

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
+import Login from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
