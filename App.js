ede669074b2cbf91bc7b6e068b09db1bc0680790
1sh1vam <shivam07.mt@gmail.com>
2023-01-14 18:32:04 +0530

    setting up the tailwind

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the signal app.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-500">Tailwind config</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
