b0b6125d0d6cdb2ff1a3dd847e7ba5f146576538
1sh1vam <shivam07.mt@gmail.com>
2023-01-16 20:42:48 +0530

    login screen

--- StyledComponents/index.js
+++ StyledComponents/index.js
@@ -1,5 +1,7 @@
-import { Image } from "@rneui/base";
+import { Image, Button, Input } from "@rneui/base";
 import { styled } from "nativewind";
 
 
 export const StyledImage = styled(Image);
+export const StyledButton = styled(Button);
+export const StyledInput = styled(Input);
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,26 +1,41 @@
 import { Button } from '@rneui/base';
-import React from 'react';
-import { KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
+import { StyledButton, StyledImage, StyledInput } from '../StyledComponents';
 
 const LoginScreen = () => {
+  const [emailId, setEmailId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = () => {
+
+  }
+
   return (
     <KeyboardAvoidingView
+      keyboardVerticalOffset={80}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       className="flex-1 items-center justify-center bg-white p-5"
     >
-      <StyledImage className="w-40 h-40" source={require('../assets/signal.png')} />
-      <TextInput
+      <StyledImage className="w-40 h-40 rounded-lg" source={require('../assets/signal.png')} />
+      <StyledInput
+        autoFocus
         textContentType="emailAddress"
-        className="border-b h-10 border-blue-500 w-[300px] mt-10 px-2 text-lg"
+        className="h-10 w-[300px] mt-10 px-1 text-lg"
         placeholder="Enter your email"
+        value={emailId}
+        onChangeText={setEmailId}
       />
-      <TextInput
+      <StyledInput
         secureTextEntry
         textContentType="password"
-        className="border-b h-10 border-blue-500 w-[300px] mt-3 px-2 text-lg"
+        className="h-10 w-[300px] mt-2 px-1 text-lg"
         placeholder="Enter your password"
+        value={password}
+        onChangeText={setPassword}
       />
-      <Button className="mt-2.5 w-40">Login</Button>
+      <StyledButton onPress={signIn} className="mt-2.5 w-52">Login</StyledButton>
+      <StyledButton type="outline" className="mt-2.5 w-52">Register</StyledButton>
     </KeyboardAvoidingView>
   );
 };
