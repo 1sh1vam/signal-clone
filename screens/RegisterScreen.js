3ff1cd6aadf7e5df7bf55b1a50722f03685cb58b
1sh1vam <shivam07.mt@gmail.com>
2023-01-17 20:36:49 +0530

    registration button

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyledButton, StyledInput, StyledText } from '../StyledComponents';
+import { useNavigation } from '@react-navigation/native';
 
 const RegisterScreen = () => {
+  const [name, setName] = useState('');
+  const [emailId, setEmailId] = useState('');
+  const [password, setPassword] = useState('');
+  const [imageUrl, setImageUrl] = useState('')
+
+  const navigation = useNavigation();
+
+  const signIn = () => {};
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={80}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      className="flex-1 items-center bg-white p-5"
+    >
+      <StyledText h3>Create a Signal account</StyledText>
+      <StyledInput
+        autoFocus
+        className="h-10 w-[300px] mt-10 px-1 text-lg"
+        placeholder="Enter your full name"
+        value={name}
+        onChangeText={setName}
+      />
+      <StyledInput
+        textContentType="emailAddress"
+        className="h-10 w-[300px] px-1 text-lg"
+        placeholder="Enter your email"
+        value={emailId}
+        onChangeText={setEmailId}
+      />
+      <StyledInput
+        secureTextEntry
+        textContentType="password"
+        className="h-10 w-[300px] px-1 text-lg"
+        placeholder="Enter your password"
+        value={password}
+        onChangeText={setPassword}
+      />
+      <StyledInput
+        secureTextEntry
+        className="h-10 w-[300px] px-1 text-lg"
+        placeholder="Enter image url"
+        value={imageUrl}
+        onChangeText={setImageUrl}
+      />
+      <StyledButton onPress={signIn} buttonStyle={styles.button}>
+        Login
+      </StyledButton>
+      <StyledButton
+        onPress={() => navigation.navigate('Register')}
+        type="outline"
+        buttonStyle={styles.button}
+      >
+        Register
+      </StyledButton>
+    </KeyboardAvoidingView>
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button: {
+    width: 200,
+    marginTop: 10
+  }
+});
