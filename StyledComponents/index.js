31efaac4674260f8f6640b5e281ff340999e513b
1sh1vam <shivam07.mt@gmail.com>
2023-01-17 20:35:43 +0530

    fixing the login screen button size

--- StyledComponents/index.js
+++ StyledComponents/index.js
@@ -1,7 +1,7 @@
-import { Image, Button, Input } from "@rneui/base";
+import { Image, Button, Input, Text } from "@rneui/base";
 import { styled } from "nativewind";
 
-
+export const StyledText = styled(Text);
 export const StyledImage = styled(Image);
 export const StyledButton = styled(Button);
 export const StyledInput = styled(Input);
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button } from '@rneui/base';
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, Platform, TextInput, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
 import { StyledButton, StyledImage, StyledInput } from '../StyledComponents';
+import {  } from 'nativewind';
 
 const LoginScreen = () => {
   const [emailId, setEmailId] = useState('');
@@ -10,9 +11,7 @@ const LoginScreen = () => {
 
   const navigation = useNavigation();
 
-  const signIn = () => {
-
-  }
+  const signIn = () => {};
 
   return (
     <KeyboardAvoidingView
@@ -20,7 +19,11 @@ const LoginScreen = () => {
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       className="flex-1 items-center justify-center bg-white p-5"
     >
-      <StyledImage className="w-40 h-40 rounded-lg" source={require('../assets/signal.png')} />
+      <StatusBar style="light" />
+      <StyledImage
+        className="w-40 h-40 rounded-lg"
+        source={require('../assets/signal.png')}
+      />
       <StyledInput
         autoFocus
         textContentType="emailAddress"
@@ -37,10 +40,25 @@ const LoginScreen = () => {
         value={password}
         onChangeText={setPassword}
       />
-      <StyledButton onPress={signIn} className="mt-2.5 w-52">Login</StyledButton>
-      <StyledButton onPress={() => navigation.navigate('Register')} type="outline" className="mt-2.5 w-52">Register</StyledButton>
+      <StyledButton onPress={signIn} buttonStyle={styles.button}>
+        Login
+      </StyledButton>
+      <StyledButton
+        buttonStyle={styles.button}
+        onPress={() => navigation.navigate('Register')}
+        type="outline"
+      >
+        Register
+      </StyledButton>
     </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  button: {
+    width: 200,
+    marginTop: 10
+  }
+})
