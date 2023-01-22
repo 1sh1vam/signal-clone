ee0f01a1892748bfd80101a928c749eeac352ca0
1sh1vam <shivam07.mt@gmail.com>
2023-01-22 19:43:09 +0530

    home screen right icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,8 @@ import { Pressable, ScrollView, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import CustomListItem from '../components/CustomListItem'
 import { Avatar } from '@rneui/base'
-import useAuthListener from '../hooks/useAuthListener'
+import { AntDesign, SimpleLineIcons } from 'react-native-vector-icons';
+import useAuthListener from '../hooks/useAuthListener';
 import { signOut } from 'firebase/auth'
 import { auth } from '../lib/firebase'
 
@@ -26,6 +27,16 @@ const HomeScreen = ({ navigation }) => {
         <Pressable onPress={signOutUser} className="ml-4 active:opacity-70">
           <Avatar rounded source={{ uri: user?.photoURL }} />
         </Pressable>
+      ),
+      headerRight: () => (
+        <View className="w-20 mr-4 flex-row justify-between">
+          <Pressable className="active:opacity-60">
+            <AntDesign name="camerao" size={24} color="black" />
+          </Pressable>
+          <Pressable className="active:opacity-60">
+            <SimpleLineIcons name="pencil" size={24} color="black" />
+          </Pressable>
+        </View>
       )
     })
   }, [user]);
