1a3d78e3fbad49f753689d6b593f19d8852cccbe
1sh1vam <shivam07.mt@gmail.com>
2023-01-22 11:28:01 +0530

    create user for firebase

---
+++ services/auth.js
@@ -0,0 +1,10 @@
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from '../lib/firebase';
+
+export const signUp = async (emailId, pwd, name, photoUrl) => {
+    const user = await createUserWithEmailAndPassword(auth, emailId, pwd);
+    await updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photoUrl,
+    });
+}
