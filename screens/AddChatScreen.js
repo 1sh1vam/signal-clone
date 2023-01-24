d53aefeea7eae1e3d2e76bf6c79e4137f98b3f2d
1sh1vam <shivam07.mt@gmail.com>
2023-01-24 21:42:12 +0530

    adding the chat to firebase

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useLayoutEffect, useState } from 'react'
 import { AntDesign } from 'react-native-vector-icons';
 import { StyledButton, StyledInput } from '../StyledComponents';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../lib/firebase';
 
 const AddChatScreen = ({ navigation }) => {
   const [chatName, setChatName] = useState('');
@@ -12,6 +14,15 @@ const AddChatScreen = ({ navigation }) => {
     });
   }, [navigation]);
 
+  const onCreateChat = async () => {
+    try {
+      await addDoc(collection(db, 'chats'), { chatName: chatName });
+      navigation.goBack();
+    } catch(err) {
+      alert('Failed to create the chat ' + err.message);
+    }
+  }
+
   return (
     <View className="p-7 h-full">
       <StyledInput
@@ -20,8 +31,9 @@ const AddChatScreen = ({ navigation }) => {
         value={chatName}
         leftIcon={<AntDesign name="wechat" color="#000" size={24} />}
         onChangeText={setChatName}
+        onSubmitEditing={onCreateChat}
       />
-      <StyledButton>Create a new chat</StyledButton>
+      <StyledButton onPress={onCreateChat}>Create a new chat</StyledButton>
     </View>
   )
 }
