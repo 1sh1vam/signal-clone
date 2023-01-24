b365a7d2b968c8c2ed2fc4db60489606fb25c1c1
1sh1vam <shivam07.mt@gmail.com>
2023-01-24 21:35:20 +0530

    add chat screen input for the chat name

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
+import { AntDesign } from 'react-native-vector-icons';
+import { StyledButton, StyledInput } from '../StyledComponents';
 
 const AddChatScreen = ({ navigation }) => {
+  const [chatName, setChatName] = useState('');
   useLayoutEffect(() => {
     navigation.setOptions({
         title: 'Add a new chat',
@@ -10,8 +13,15 @@ const AddChatScreen = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <View>
-      <Text>AddChatScreen</Text>
+    <View className="p-7 h-full">
+      <StyledInput
+        autoFocus
+        placeholder="Enter the chat name"
+        value={chatName}
+        leftIcon={<AntDesign name="wechat" color="#000" size={24} />}
+        onChangeText={setChatName}
+      />
+      <StyledButton>Create a new chat</StyledButton>
     </View>
   )
 }
