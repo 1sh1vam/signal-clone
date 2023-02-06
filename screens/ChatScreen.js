6a63ba8755b1a17536bd754e89a5c0df4e8935b6
1sh1vam <shivam07.mt@gmail.com>
2023-02-06 20:20:29 +0530

    chat screen header

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { AntDesign, FontAwesome, Ionicons } from 'react-native-vector-icons'
+import { Avatar } from '@rneui/base';
+import { StyledButton } from '../StyledComponents'
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route }) => {
+  const {  } = route.params;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Chat',
+      headerTitleAlign: 'left',
+      headerTitle: () => (
+        <View className="flex-row items-center flex-1">
+          <Avatar rounded source={{ uri: 'https://cdn-icons-png.flaticon.com/512/847/847969.png' }} />
+          <Text className="text-white font-bold ml-2.5">Cenc</Text>
+        </View>
+      ),
+      headerLeft: () => (
+        <StyledButton buttonStyle={styles.headerButtonStyle}>
+          <AntDesign name="arrowleft" size={24} color="#fff" />
+        </StyledButton>
+      ),
+      headerRight: () => (
+        <View className="flex-row gap-2">
+          <StyledButton buttonStyle={styles.headerButtonStyle}>
+            <FontAwesome name="video-camera" size={24} color="#fff" />
+          </StyledButton>
+          <StyledButton buttonStyle={styles.headerButtonStyle}>
+            <Ionicons name="call" size={24} color="#fff" />
+          </StyledButton>
+        </View>
+      )
+    })
+  }, []);
+
   return (
     <View>
       <Text>ChatScreen</Text>
@@ -11,4 +43,8 @@ const ChatScreen = () => {
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerButtonStyle: {
+    backgroundColor: 'transparent'
+  }
+})
