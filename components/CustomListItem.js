4e320c1a40996822c31db0bcdc32a593c373b3ca
1sh1vam <shivam07.mt@gmail.com>
2023-02-14 20:39:36 +0530

    fetxhing the latest chat in chat list card

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar, ListItem } from '@rneui/base'
+import { collection, limit, onSnapshot, orderBy, query, where } from 'firebase/firestore';
+import { db } from '../lib/firebase';
 
-const CustomListItem = ({ chatName, desc, photoUrl, enterChat }) => {
+const CustomListItem = ({ id, chatName, desc, photoUrl, enterChat }) => {
+  const [message, setMessage] = useState(null);
+  useEffect(
+    () =>
+      onSnapshot(query(collection(db, 'chats', id, 'messages'), orderBy('timeStamp', 'desc'), limit(1)), (doc) => {
+        if (!doc.empty) {
+            setMessage({ ...doc.docs[0].data(), id: doc.docs[0].id });
+        }
+      }),
+    []
+  );
+
   return (
     <ListItem style={{ borderTopWidth: 1, borderColor: '#ECECEC' }} onPress={enterChat}>
         <Avatar
